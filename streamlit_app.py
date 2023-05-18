5ea4591a19682518903eb352f45b7d1f05e54438
adidegal0281 <79826018+adidegal0281@users.noreply.github.com>
2023-05-18 15:26:03 +0530

    Update streamlit_app.py
    bug fix line 25(:)

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ streamlit.dataframe(fruits_to_show)
 #adding new library requests
 #New section to display frityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-def get_fruityvice_data(this_fruit_choice)
+def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
